7376ab849abcb9b24c7c76306c19eac9cac8b288
yasunori.matsuoka@gmail.com <yasunori.matsuoka@gmail.com>
2020-01-11 18:10:30 +0900

    Add status module to nem1-sdk.js close #5

--- nem1-sdk.js
+++ nem1-sdk.js
@@ -5,9 +5,11 @@ import { usualFetch } from "./nem1/utils/usualFetch.js";
 import { isHttps, isHttp, isWss, isws } from "./nem1/utils/url.js";
 import { portNo } from "./nem1/utils/portNo.js";
 import { heartbeat } from "./nem1/heartbeat.js";
+import { status } from "./nem1/status.js";
 
 export const nem1 =　{
     heartbeat: heartbeat,
+    status: status,
     utils: {
         config: config,
         integratedFetch: integratedFetch,
