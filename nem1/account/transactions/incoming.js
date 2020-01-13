f739364e074366ca2f1e679e4df0b80991ec19e9
yasunori.matsuoka@gmail.com <yasunori.matsuoka@gmail.com>
2020-01-13 09:35:17 +0900

    Modify incoming.js as ES6 module close #12

--- nem1/account/transactions/incoming.js
+++ nem1/account/transactions/incoming.js
@@ -1,14 +1,18 @@
 import { integratedFetch } from "../../utils/integratedFetch.js";
 import { portNo } from "../../utils/portNo.js";
 
-export const incoming = async (nodeUrl, rawAddress, hash, id) => {
+export const incoming = async (nodeUrl, rawAddress, hashOrId, id) => {
     const address = rawAddress; //Todo: Validate address here
     let url = nodeUrl + ":" + portNo(nodeUrl) + "/account/transfers/incoming?address=" + address;
-    if(hash !== undefined){
-        url = url + "&hash=" + hash;
-    }
-    if(id !== undefined){
-        url = url + "&id=" + id;
+    if(hashOrId !== undefined){
+        if(Number.isInteger(Number(hashOrId))){
+            url = url + "&id=" + Number(hashOrId);
+        }else{
+            url = url + "&hash=" + hashOrId;
+        }
+        if((id !== undefined) && (Number.isInteger(Number(id)))){
+            url = url + "&id=" + id;
+        }
     }
     const result = await integratedFetch(url);
     return result;
